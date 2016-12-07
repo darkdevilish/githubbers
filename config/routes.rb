cf16326a859c500c7e803e4817805c9219425602
Anthony Gonzalez <darkdevilishag@gmail.com>
2016-12-06 20:18:19 -0500

    Adds UI and route to search action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'github_users#index'
+
+  get 'search', to: 'github_users#search'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
