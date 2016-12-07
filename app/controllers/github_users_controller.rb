d9c5ea3b27d97b5e380155a5d221699350e9c2e6
Anthony Gonzalez <darkdevilishag@gmail.com>
2016-12-06 21:47:08 -0500

    Connects to github api and responds basic user info

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -3,6 +3,11 @@ class GithubUsersController < ApplicationController
   end
 
   def search
+    client_id = "485994f65f0cca2d779a"
+    client_secret = "8f7cd14c67e2d5f8e75a35fe165802c22945ea3b"
+    github = Github.new client_id: client_id, client_secret: client_secret
+    @user = github.users.get user: params[:user]
+
     respond_to do |format|
       format.html { redirect_to root_path}
       format.js
