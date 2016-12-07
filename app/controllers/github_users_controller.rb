f331f05a79ca6a79f56151a996aa2991413b5d2b
Anthony Gonzalez <darkdevilishag@gmail.com>
2016-12-06 23:38:42 -0500

    Adds environment variables

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -3,8 +3,8 @@ class GithubUsersController < ApplicationController
   end
 
   def search
-    client_id = "485994f65f0cca2d779a"
-    client_secret = "8f7cd14c67e2d5f8e75a35fe165802c22945ea3b"
+    client_id = ENV["CLIENT_ID"]
+    client_secret = ENV["CLIENT_SECRET"]
     github = Github.new client_id: client_id, client_secret: client_secret
     @user = github.users.get user: params[:user]
     @user_repos = github.repos.list user: params[:user]
