5af6710c9a87af57d47bb6418ff2803284c90f49
Anthony Gonzalez <darkdevilishag@gmail.com>
2016-12-06 20:45:35 -0500

    Corrects form method, route method, responds with js

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -1,4 +1,11 @@
 class GithubUsersController < ApplicationController
   def index
   end
+
+  def search
+    respond_to do |format|
+      format.html { redirect_to root_path}
+      format.js
+    end
+  end
 end
